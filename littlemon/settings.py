a0fb8d9aa5c406114aedf9362fa9b0562870964b
fidelp27 <fidel2702@gmail.com>
2024-04-01 14:58:53 -0300

    add home template and bbdd mysql configuration

--- littlemon/settings.py
+++ littlemon/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'littlemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon_final_project',
+        'USER': 'root',
+        'PASSWORD':'Ezedel1488.',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+        }
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    def __init__(self, id, title, price, description, image, inventory):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.inventory = inventory
+        self.description = description
+        self.image = image
+
+    def __str__(self):
+        return self.name + " " + self.price + " " + self.description + " " + self.inventory
+
+class Reservation(models.Model):
+    def __init__(self, id, name, no_of_guest, booking_date):
+        self.id = id
+        self.name = name
+        self.no_of_guest = no_of_guest
+        self.booking_date = booking_date
+        
+
+    def __str__(self):
+        return self.name + " " + self.booking_date + " " + self.no_of_guest + " " + self.id
