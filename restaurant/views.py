d249d1bcdaf85481ecac69b10341e8179f38fb41
fidelp27 <fidel2702@gmail.com>
2024-04-02 10:38:15 -0300

    add api configuration and serializers to do menu and reservations

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .serializers import MenuSerializer, ReservationSerializer
+from rest_framework.decorators import api_view
+from rest_framework import generics, viewsets
+from .models import Menu, Reservation
+
 # Create your views here.
 def home(request):
     return render(request, 'restaurant/home.html')
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    
+    
