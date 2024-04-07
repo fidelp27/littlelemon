130e2812b9cb0dadfc900bad07b9cc5dde4804ed
fidelp27 <fidel2702@gmail.com>
2024-04-07 08:49:01 -0300

    test basic

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,25 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Menu(models.Model):
-    def __init__(self, id, title, price, description, image, inventory, category):
-        self.id = id
-        self.title = title
-        self.price = price
-        self.inventory = inventory
-        self.description = description
-        self.image = image
-        self.category = category
-        
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, default="No title")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    inventory = models.IntegerField(default=0)
+    description = models.TextField(max_length=500, default="No description available")
+    image = models.URLField(max_length= 200, default="https://www.google.com")
+    category = models.CharField(max_length=100, default="Uncategorized")       
 
     def __str__(self):
         return self.title + " " + self.price + " " + self.description + " " + self.inventory
 
 class Reservation(models.Model):
-    def __init__(self, id, name, no_of_guest, booking_date):
-        self.id = id
-        self.name = name
-        self.no_of_guest = no_of_guest
-        self.booking_date = booking_date 
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default="No name")
+    no_of_guest = models.IntegerField(default=0)
+    booking_date = models.DateField(default="2021-01-01") 
 
     def __str__(self):
         return self.name + " " + self.booking_date + " " + self.no_of_guest + " " + self.id
--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Menu
+
+class MenuTestCase(TestCase):
+    def setUp(self):
+        Menu.objects.create(title="Pizza", price=12.50, inventory=10, description="Pizza de pepperoni", image="https://www.google.com", category="Entrees")
+        Menu.objects.create(title="Pasta", price=8.50, inventory=10, description="Pasta de alfredo", image="https://www.google.com", category="Entrees")
+        
+    def test_menu_items(self):
+        pizza = Menu.objects.get(title="Pizza")
+        pasta = Menu.objects.get(title="Pasta")
+        self.assertEqual(pizza.price, 12.50)
+        self.assertEqual(pasta.price, 8.50)
