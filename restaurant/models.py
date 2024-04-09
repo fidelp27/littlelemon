4b1a5a6b9f11bd1fec37d9298c86e8d57391f9a9
fidelp27 <fidel2702@gmail.com>
2024-04-09 09:11:22 -0300

    delete reservation app to change to apiapp

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default="Uncategorized")
+
+    def __str__(self):
+        return self.name
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, default="No title")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    inventory = models.IntegerField(default=0)
     description = models.TextField(max_length=500, default="No description available")
-    image = models.URLField(max_length= 200, default="https://www.google.com")
-    category = models.CharField(max_length=100, default="Uncategorized")       
+    image = models.ImageField(upload_to='menu_images', default='{%static "assets/menu_images"%}')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)      
 
     def __str__(self):
         return self.title + " " + self.price + " " + self.description + " " + self.inventory
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('signin/', views.signin, name='signin'),
     path('logoff/', views.logoff, name='logoff'),
-    path('menu/', views.MenuItemsView.as_view(), name='menu_items'),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single_menu_item'),
     path('api-token-auth/', obtain_auth_token),
+    path('menu/', views.MenuItemsView.as_view(), name='menu_items'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single_menu_item'),    
 ]
